5dbe032b8b3edf88fb6ca6787cd0d1d0e13869af
Justin Sitter <justin@jaytria.com>
2019-03-26 10:38:49 -0700

    fix cors restriction

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', (req, res)=>{
   fs.readFile('./datasets/saved.csv', 'utf8', function(err, contents){
     if(err){
@@ -12,7 +16,7 @@ app.get('/', (req, res)=>{
     }
 
   })
-  
+
 })
 
 app.set('port', process.env.PORT || 8070);
